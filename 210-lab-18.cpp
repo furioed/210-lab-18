71a3100c82d75e08be551ece4a1cd6f7ece621b9
Mamadou Sissoko <msissoko2317@gmail.com>
2025-10-05 13:35:57 -0700

    Lab 18 Update 2

--- 210-lab-18.cpp
+++ 210-lab-18.cpp
@@ -19,6 +19,17 @@ void addHead(Node*& head, double rating, const string& comment) {
     head = newNode;
 }
 
+// Adds review at tail of linked list
+void addTail(Node*& head, double rating, const string& comment) {
+    Node* newNode = new Node{rating, comment, nullptr};
+    if (!head) head = newNode;
+    else {
+        Node* curr = head;
+        while (curr->next) curr = curr->next;
+        curr->next = newNode;
+    }
+}
+
 // Displays all reviews
 void display(Node* head) {
     cout << "\nOutputting all reviews:\n";
@@ -32,6 +43,15 @@ void display(Node* head) {
 
 int main() {
     Node* head = nullptr;
+    int choice;
+
+    cout << "Which linked list method should we use?\n";
+    cout << "    [1] New nodes are added at the head of the linked list\n";
+    cout << "    [2] New nodes are added at the tail of the linked list\n";
+    cout << "    Choice: ";
+    cin >> choice;
+    cin.ignore();
+
     char again = 'y';
 
     // User input starts here
@@ -45,7 +65,8 @@ int main() {
         cout << "Enter review comments: ";
         getline(cin, comment);
 
-        addHead(head, rating, comment);
+        if (choice == 1) addHead(head, rating, comment);
+        else addTail(head, rating, comment);
 
         cout << "Enter another review? Y/N: ";
         cin >> again;
